2bff2fc5cca468ce9b2f4004f1c0c42e08621f2b
Christian Bongard <christian.bongard90@gmail.com>
2023-12-07 13:34:26 +0400

    Test message

---
+++ workshop.rb
@@ -0,0 +1,15 @@
+age = 17
+
+answer = age >= 18 ? "You Can Vote" : "You Cannot Vote"
+
+puts answer
+
+def square(num)
+  num * num
+end
+
+puts square(2)
+puts square(23)
+puts square(5)
+puts square(6)
+puts square(2000)
